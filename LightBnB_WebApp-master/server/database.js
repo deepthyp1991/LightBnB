47e72b7e70da99f7b8f7cf4b216926275151ab19
deepthyp1991 <deepthyp1991@gmail.com>
2021-01-08 00:08:05 +0000

    queried all reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -22,7 +22,7 @@ const getUserWithEmail = function(email) {
   SELECT * FROM users
   WHERE email = $1;
   `,[email])
-  .then(res => {return res.rows[0]})
+  .then(res => res.rows[0])
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +36,7 @@ const getUserWithId = function(id) {
   SELECT * FROM users
   WHERE id = $1`,
   [id])
-  .then(res => {return res.row[0]})
+  .then(res => res.row[0])
 }
 exports.getUserWithId = getUserWithId;
 
@@ -51,7 +51,7 @@ const addUser =  function(user) {
   INSERT INTO users(name, email, password)
   VALUES ($1,$2,$3)
   RETURNING *;`, [user.name,user.email,user.password])
-  .then(res => {return res.row[0]});
+  .then(res =>  res.row[0]);
 }
 exports.addUser = addUser;
 
@@ -63,7 +63,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+  `, [guest_id,limit])
+  .then(res => res.rows)
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
